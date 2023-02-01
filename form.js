69f284f4c3a5a26e01d8ca093acc5e405c6c2ee0
Olivier F <94624011+OlivierFacelina@users.noreply.github.com>
2023-02-01 09:23:16 +0100

    Correction

--- form.js
+++ form.js
@@ -10,37 +10,45 @@ var nw = new Vue( {
         picked: "",
         checkedCompetences: [],
         button: "Vous avez bien envoyé votre formulaire ! Merci à vous.",
-        error: null,
+        error: [],
+        arrayofComp: ["1","2","3","4"]
     },
 
+    
     methods: {
-        filter: function() {
-            let test = this.lastname.match(/^[A-z]+$/)
+        filter: function(e) {
+            let value = e.target.value
+            let id = e.target.id
+            let test = value.match(/^[A-z]+$/)
             if(!test){
-            this.error = "Ce nom n'est pas au bon format"
+            let error = {
+                name: id,
+                error: "Ce champ est incorrect"
             }
+            this.error = [...this.error,error] // ... = copier tableau (récupérer tous les éléments)
+        }
+        else {
+            
+        }
+    },
+    
+    submit: function() {
+        console.log(this.lastname, this.firstname, this.email, this.selection, this.message, this.picked, this.checkedCompetences)
         },
         
-        submit: function() {
-            console.log(this.lastname, this.firstname, this.email, this.selection, this.message, this.picked, this.checkedCompetences)
+    resetForm() {
+        this.$refs.form.reset();
         },
 
-        resetForm() {
-            this.$refs.form.reset();
-          },
+    },
 
-        test: function disableSubmit() {
-        document.getElementById("submit").disabled = true;
-        },
-  
-        test: function activateButton(element) {   
-            if(element.checked) {
-            document.getElementById("submit").disabled = false;
-            }
-            else  {
-            document.getElementById("submit").disabled = true;
-            }
-    
-    }
-    }
+    mounted: function(){
+        let container = document.querySelector('#array')
+        let input = ""
+        this.arrayofComp.map((item)=>{
+            input += `<div>${item}</div>`
+            console.log(item)
+        })
+        container.innerHTML = input
+    },
 })
